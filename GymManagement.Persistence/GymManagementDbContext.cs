c004d00b8d46f5981d16796cc79bb2cd040e437c
VukBakic <bakic.vuk@gmail.com>
2024-02-21 16:47:41 +0100

    Adding Persistence layer and DbContext

---
+++ GymManagement.Persistence/GymManagementDbContext.cs
@@ -0,0 +1,40 @@
+﻿using GymManagement.Domain;
+using GymManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.Persistence
+{
+    internal class GymManagementDbContext : DbContext
+    {
+        public GymManagementDbContext(DbContextOptions<GymManagementDbContext> options) :base(options) 
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(GymManagementDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.DateModified = DateTime.Now;
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<ExerciseType> ExercisesTypes { get; set; }
+    }
+}
